2e564cfea342b003e61974c3c9694803f292cc66
Snapman7 <renat_ja02@mail.ru>
2022-05-27 15:43:54 +0300

    implemented camera full and reworked rasterizer_renderer.cpp

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -36,6 +36,15 @@ void cg::renderer::rasterization_renderer::init()
 void cg::renderer::rasterization_renderer::render()
 {
 	rasterizer->clear_render_target({1, 255, 112});
+    float4x4 matrix = mul(
+            camera->get_projection_matrix(),
+            camera->get_view_matrix(),
+            model->get_world_matrix()
+    );
+    rasterizer->vertex_shader = [&](float4 vertex, cg::vertex vertex_data) {
+      auto processed = mul(matrix, vertex);
+      return std::make_pair(processed, vertex_data);
+    };
 
 	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); ++shape_id) {
 		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
@@ -43,16 +52,6 @@ void cg::renderer::rasterization_renderer::render()
         rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
 
     }
-
-	float4x4 matrix = mul(
-			camera->get_projection_matrix(),
-			camera->get_view_matrix(),
-			model->get_world_matrix()
-			);
-	rasterizer->vertex_shader = [&](float4 vertex, cg::vertex vertex_data) {
-		auto processed = mul(matrix, vertex);
-		return std::make_pair(processed, vertex_data);
-	};
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
 
 	cg::utils::save_resource(*render_target, settings->result_path);
