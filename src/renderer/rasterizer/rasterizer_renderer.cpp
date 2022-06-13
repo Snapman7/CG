62db07182e0685d15489bddf3c9d8206b3d99428
Snapman7 <renat_ja02@mail.ru>
2022-06-13 15:05:58 +0300

    Changed camera position and background

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -37,7 +37,7 @@ void cg::renderer::rasterization_renderer::init()
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	rasterizer->clear_render_target({255, 255, 255});
+	rasterizer->clear_render_target({0, 0, 0});
     float4x4 matrix = mul(
             camera->get_projection_matrix(),
             camera->get_view_matrix(),
