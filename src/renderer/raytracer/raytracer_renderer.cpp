89c57f40bf746e05e46158436e44311cc47d02dd
Snapman7 <renat_ja02@mail.ru>
2022-06-21 19:20:46 +0300

    Adjust ray_tracing_renderer class to build the acceleration structure

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -104,5 +104,4 @@ void cg::renderer::ray_tracing_renderer::render()
 	std::cout << "Raytracing took " << raytracing_duration.count() << " ms\n";
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO: Lab 2.05. Adjust ray_tracing_renderer class to build the acceleration structure
 }
